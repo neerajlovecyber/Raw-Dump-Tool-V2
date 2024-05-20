3ecc26fe0b5c76f63c59627fa1383a23c7c50edf
Neeraj singh <nirmalapanghal43@gmail.com>
2024-05-20 18:38:36 +0530

    v2 INTAIL bUILD

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,29 @@
 fn main() {
-    tauri_build::build()
+    let windows_attributes = tauri_build::WindowsAttributes::new().app_manifest(
+        r#"
+    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+        type="win32"
+        name="Microsoft.Windows.Common-Controls"
+        version="6.0.0.0"
+        processorArchitecture="*"
+        publicKeyToken="6595b64144ccf1df"
+        language="*"
+      />
+    </dependentAssembly>
+  </dependency>
+      <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+          <security>
+              <requestedPrivileges>
+                  <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+              </requestedPrivileges>
+          </security>
+      </trustInfo>
+    </assembly>
+    "#,
+    );
+    let attributes = tauri_build::Attributes::new().windows_attributes(windows_attributes);
+    tauri_build::try_build(attributes).unwrap();
 }
